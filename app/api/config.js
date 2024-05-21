4ae52af23192a4c9c231e465455734263eabb2ef
SaldNevo <119350210+Arteem66@users.noreply.github.com>
2024-05-21 16:47:47 +0300

    fix bug

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindiebacksaldnevo.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://pindiefrontsaldnevo.nomoredomainswork.ru'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
