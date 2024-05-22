2e443f9ae476d41820671f9b7270d7dd4a1c7eea
SaldNevo <119350210+Arteem66@users.noreply.github.com>
2024-05-22 17:14:41 +0300

    back url

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'https://pindiebacksaldnevo.nomoredomainswork.ru/api'
+export const BASE_URL =
+	'https://pindiebacksaldnevo.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
